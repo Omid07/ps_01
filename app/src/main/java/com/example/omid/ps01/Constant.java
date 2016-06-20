99fe76691cffc14f74c71f80bf12a7cb0d262768
Omid07 <omidchowdhury@gmail.com>
2016-06-20 16:16:58 +0600

    select image

---
+++ app/src/main/java/com/example/omid/ps01/Constant.java
@@ -0,0 +1,9 @@
+package com.example.omid.ps01;
+
+/**
+ * Created by omid on 6/20/16.
+ */
+public class Constant {
+    public static final int CAMERA_REQUEST = 1;
+    public static final int GALLERY_REQUEST = 2;
+}
--- app/src/main/java/com/example/omid/ps01/MainActivity.java
+++ app/src/main/java/com/example/omid/ps01/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.omid.ps01;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButtonCamera;
+    private Button mButtonGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonCamera = (Button) findViewById(R.id.button_camera);
+        mButtonGallery = (Button) findViewById(R.id.button_gallery);
+        mButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera();
+            }
+        });
+        mButtonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gallery();
+            }
+        });
+    }
+
+    public void camera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, Constant.CAMERA_REQUEST);
+    }
+
+    public void gallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media
+                .EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, Constant.GALLERY_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == Constant.CAMERA_REQUEST) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get(getString(R.string.data));
+            Intent intent = new Intent(MainActivity.this, EditImageActivity.class);
+            intent.putExtra(getString(R.string.bitmap_image), bitmap);
+            startActivity(intent);
+        } else if (requestCode == Constant.GALLERY_REQUEST) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null,
+                    null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            Intent intent = new Intent(MainActivity.this, EditImageActivity.class);
+            intent.putExtra(getString(R.string.image_path), picturePath);
+            startActivity(intent);
+        }
     }
 }
+
